17f289ce1a0d21c7b220bf1bd355f672a2d7c652
jorge jardines <jorge.jardines@frontiersin.org>
2015-03-10 01:04:49 +0100

    add missing data to digest and add arxiv_id as script param

--- arxiv_digest.py
+++ arxiv_digest.py
@@ -3,6 +3,7 @@
 
 import sqlite3
 from xml.dom import minidom
+import sys
 
 
 def parse_arxiv_entry(entry):
@@ -12,13 +13,14 @@ def parse_arxiv_entry(entry):
     :return: dictionary with publication data
     """
     url = entry.getElementsByTagName('id')[0].childNodes[0].data
+    tokens = url.split('/')
+    arxiv_id = tokens[len(tokens) - 1]
     doi_elem = entry.getElementsByTagName('arxiv:doi')
-    doi = None
+    doi = ""
     if doi_elem:
         doi = doi_elem[0].childNodes[0].data
-    title = entry.getElementsByTagName('title')[0].childNodes[0].data.encode('ascii', 'ignore')\
-        .replace('\n', ' ')
-    abstract = entry.getElementsByTagName('summary')[0].childNodes[0].data.encode('ascii', 'ignore')\
+    title = entry.getElementsByTagName('title')[0].childNodes[0].data.replace('\n', ' ')
+    abstract = entry.getElementsByTagName('summary')[0].childNodes[0].data\
         .replace('\n', ' ').lstrip()
     published_date = entry.getElementsByTagName('published')[0].childNodes[0].data
     updated_date = entry.getElementsByTagName('updated')[0].childNodes[0].data
@@ -30,9 +32,7 @@ def parse_arxiv_entry(entry):
         authors.append(
             {
                 "name": author.getElementsByTagName('name')[0].childNodes[0].data,
-                "affiliation": affiliation
-                                and affiliation[0].childNodes[0].data
-                                or ""
+                "affiliation": affiliation and affiliation[0].childNodes[0].data or ""
             }
         )
     journal_ref = entry.getElementsByTagName('arxiv:journal_ref')
@@ -41,13 +41,14 @@ def parse_arxiv_entry(entry):
         journal = journal_ref[0].childNodes[0].data
 
     full_text = ""
-    links = entry.getElementsByTagName('links') or []
+    links = entry.getElementsByTagName('link') or []
     for link in links:
         if link.hasAttribute('title') and link.getAttribute('title') == 'pdf':
             full_text = link.getAttribute('href')
             break
 
     return {
+        'arxiv_id': arxiv_id,
         'doi': doi,
         'title': title,
         'abstract': abstract,
@@ -56,8 +57,9 @@ def parse_arxiv_entry(entry):
         'updated': updated_date,
         'url': url,
         'journal': journal,
-        'full_text':full_text
+        'full_text': full_text
     }
+
 
 def digest_arxiv_entry(source):
     """
@@ -66,41 +68,42 @@ def digest_arxiv_entry(source):
     :return: list of articles
     """
     xml_data = minidom.parseString(source.encode('utf-8'))
-    articles = []
     entries = xml_data.getElementsByTagName('entry')
-    for entry in entries:
-        article = parse_arxiv_entry(entry)
-        articles.append(article)
-    return articles
+    if entries:
+        article = parse_arxiv_entry(entries[0])
+        return article
 
 
 def select_top_ten_raw_data():
     rows = []
     with sqlite3.connect('arxiv_crawler.db') as conn:
+        conn.row_factory = sqlite3.Row
         c = conn.cursor()
-        c.execute('select * from raw_data limit 100')
+        c.execute('select arxiv_id, data from raw_data limit 10')
         rows = c.fetchall()
     return rows
 
 
 def select_raw_data_by_id(arxiv_id):
-    conn = sqlite3.connect('arxiv_crawler.db')
-    try:
+    """
+    Return the arxiv data for the specified id
+    :param arxiv_id: Id of arxiv
+    :return: dictionary with the arxiv_id and data
+    :rtype: dict
+    """
+    with sqlite3.connect('arxiv_crawler.db') as conn:
+        conn.row_factory = sqlite3.Row
         c = conn.cursor()
-        c.execute('select arxiv_id, data from raw_data limit 1')
+        c.execute('select arxiv_id, data from raw_data where arxiv_id=:Id limit 1', {"Id": arxiv_id})
         row = c.fetchone()
         return row
-    except Exception:
-        raise
-    finally:
-        conn.close()
 
 
 def main():
-    rows = select_top_ten_raw_data()
-    for row in rows:
-        result = digest_arxiv_entry(row[1])
-        print result
+    #TODO: add argument parser
+    row = select_raw_data_by_id(sys.argv[1])
+    result = digest_arxiv_entry(row['data'])
+    print result
 
-
-main()
+if __name__ == "__main__":
+    main()
